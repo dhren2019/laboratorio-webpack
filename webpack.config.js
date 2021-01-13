3a5fd2406680802e2d1525d4090e76014bf32b99
[Rodrigo] <[roglobar10@gmail.com]>
2021-01-13 19:51:16 +0100

    First Commit

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./index.html",
+  filename: "./index.html",
+  hash:true,
+});
+
+const miniCss = new MiniCssExtractPlugin({
+  filename: "[name].css",
+  chunkFilename: "[id].css"
+});
+
+module.exports = {
+  //entry: ['./main.tsx'],
+  entry: {
+    app: "./index.tsx"
+  },
+  output: {
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".css", ".scss", ".png"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|tsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass")
+            }
+          },
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [htmlPlugin, miniCss]
+};
