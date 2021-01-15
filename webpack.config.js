66eeb69fb1a3e582c66928316f259d9223fabfc6
[Rodrigo] <[roglobar10@gmail.com]>
2021-01-15 20:20:00 +0100

    Push de las 20

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|tsx)$/,
+        test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
